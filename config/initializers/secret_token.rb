0c38c5c22f64c1b19d2f13b19225d4f16685d15f
Adam Cooper <acooper@rallydev.com>
2013-06-27 17:10:38 -0600

    Created a new rails application using hartles book

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4ab5c47deef329d886f12e43bb88d65258a6d52896fa0ff60688713e8aa49318b9313c8f144420fef4db23c04a1773d1c18e19e0a663bfd3805b62023b6628d8'
